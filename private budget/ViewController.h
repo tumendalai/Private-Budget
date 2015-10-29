c91a070e0049e6f3cdc9036e705f3bc927025df0
tuguldur purevnyam <tugelder17@tuguldurs-macbook-pro.local>
2015-10-29 18:58:26 +0800

    Initial Commit

---
+++ private budget/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  private budget
+//
+//  Created by tuguldur purevnyam on 29.10.15.
+//  Copyright © 2015 tuguldur purevnyam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ private budget/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  private budget
+//
+//  Created by tuguldur purevnyam on 29.10.15.
+//  Copyright © 2015 tuguldur purevnyam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
